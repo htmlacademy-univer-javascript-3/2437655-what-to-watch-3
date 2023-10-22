5b5eaed54b6be557f71bd26319d5560e68db1129
SergeyBirukov <birukovsergej121@gmail.com>
2023-10-22 13:26:53 +0500

    Add sign-in-page

--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -1,4 +1,5 @@
 import {FilmCard, FilmCardProps} from "../../components/film-card/film-card.tsx";
+import {Footer} from "../../components/footer/footer.tsx";
 
 export type MainPageProps = {
     promoFilmTitle: string;
@@ -186,19 +187,7 @@ export function MainPage({promoFilmTitle, promoFilmGenre, promoFilmReleaseDate, 
                     </div>
                 </section>
 
-                <footer className="page-footer">
-                    <div className="logo">
-                        <a className="logo__link logo__link--light">
-                            <span className="logo__letter logo__letter--1">W</span>
-                            <span className="logo__letter logo__letter--2">T</span>
-                            <span className="logo__letter logo__letter--3">W</span>
-                        </a>
-                    </div>
-
-                    <div className="copyright">
-                        <p>© 2019 What to watch Ltd.</p>
-                    </div>
-                </footer>
+                <Footer/>
             </div>
         </>
     )
