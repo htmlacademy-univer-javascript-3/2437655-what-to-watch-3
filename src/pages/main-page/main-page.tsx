d55218c1853c7ed163391d7c40536f791c02de64
SergeyBirukov <birukovsergej121@gmail.com>
2023-10-22 13:14:27 +0500

    Move genres to separate list

--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -8,6 +8,19 @@ export type MainPageProps = {
 }
 
 export function MainPage({promoFilmTitle, promoFilmGenre, promoFilmReleaseDate, promoFilmImage} : MainPageProps): JSX.Element {
+    const genres: string[] = [
+        "All genres",
+        "Comedies",
+        "Crime",
+        "Documentary",
+        "Dramas",
+        "Horror",
+        "Kids",
+        "Family",
+        "Romance",
+        "Sci-Fi",
+        "Thrillers"
+    ]
     const filmCards: FilmCardProps[] =[
         {
             image: 'img/fantastic-beasts-the-crimes-of-grindelwald.jpg',
@@ -158,36 +171,10 @@ export function MainPage({promoFilmTitle, promoFilmGenre, promoFilmReleaseDate, 
                     <h2 className="catalog__title visually-hidden">Catalog</h2>
 
                     <ul className="catalog__genres-list">
-                        <li className="catalog__genres-item catalog__genres-item--active">
-                            <a href="#" className="catalog__genres-link">All genres</a>
-                        </li>
-                        <li className="catalog__genres-item">
-                            <a href="#" className="catalog__genres-link">Comedies</a>
-                        </li>
-                        <li className="catalog__genres-item">
-                            <a href="#" className="catalog__genres-link">Crime</a>
-                        </li>
-                        <li className="catalog__genres-item">
-                            <a href="#" className="catalog__genres-link">Documentary</a>
-                        </li>
-                        <li className="catalog__genres-item">
-                            <a href="#" className="catalog__genres-link">Dramas</a>
-                        </li>
-                        <li className="catalog__genres-item">
-                            <a href="#" className="catalog__genres-link">Horror</a>
-                        </li>
-                        <li className="catalog__genres-item">
-                            <a href="#" className="catalog__genres-link">Kids & Family</a>
-                        </li>
-                        <li className="catalog__genres-item">
-                            <a href="#" className="catalog__genres-link">Romance</a>
-                        </li>
-                        <li className="catalog__genres-item">
-                            <a href="#" className="catalog__genres-link">Sci-Fi</a>
-                        </li>
-                        <li className="catalog__genres-item">
-                            <a href="#" className="catalog__genres-link">Thrillers</a>
-                        </li>
+                        {genres.map((genre) =>
+                            <li className="catalog__genres-item catalog__genres-item--active">
+                                <a href="#" className="catalog__genres-link">{genre}</a>
+                            </li>)}
                     </ul>
 
                     <div className="catalog__films-list">
