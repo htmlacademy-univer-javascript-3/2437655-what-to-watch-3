5b39961c4c9bcf16edfafc5a369532fa160a709c
SergeyBirukov <birukovsergej121@gmail.com>
2023-10-22 14:36:32 +0500

    Add pages

--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -1,6 +1,5 @@
 import {FilmCard, FilmCardProps} from "../../components/film-card/film-card.tsx";
 import {Footer} from "../../components/footer/footer.tsx";
-import {Header} from "../../components/header/header.tsx";
 
 export type MainPageProps = {
     promoFilmTitle: string;
@@ -114,7 +113,26 @@ export function MainPage({promoFilmTitle, promoFilmGenre, promoFilmReleaseDate, 
 
                 <h1 className="visually-hidden">WTW</h1>
 
-                <Header/>
+                <header className="page-header film-card__head">
+                    <div className="logo">
+                        <a className="logo__link">
+                            <span className="logo__letter logo__letter--1">W</span>
+                            <span className="logo__letter logo__letter--2">T</span>
+                            <span className="logo__letter logo__letter--3">W</span>
+                        </a>
+                    </div>
+
+                    <ul className="user-block">
+                        <li className="user-block__item">
+                            <div className="user-block__avatar">
+                                <img src="img/avatar.jpg" alt="User avatar" width="63" height="63" />
+                            </div>
+                        </li>
+                        <li className="user-block__item">
+                            <a className="user-block__link">Sign out</a>
+                        </li>
+                    </ul>
+                </header>
 
                 <div className="film-card__wrap">
                     <div className="film-card__info">
--- src/pages/sign-in-page/sign-in-page.tsx
+++ src/pages/sign-in-page/sign-in-page.tsx
@@ -1,11 +1,16 @@
 import {Footer} from "../../components/footer/footer.tsx";
-import {Logo} from "../../components/logo/logo.tsx";
 
 export function SignInPage(): JSX.Element {
     return (
         <div className="user-page">
             <header className="page-header user-page__head">
-                <Logo/>
+                <div className="logo">
+                    <a href="main.html" className="logo__link">
+                        <span className="logo__letter logo__letter--1">W</span>
+                        <span className="logo__letter logo__letter--2">T</span>
+                        <span className="logo__letter logo__letter--3">W</span>
+                    </a>
+                </div>
 
                 <h1 className="page-title user-page__title">Sign in</h1>
             </header>
