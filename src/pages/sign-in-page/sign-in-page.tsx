803535b1a78a87081c00dcffab84659c76d7dda1
Сергей Бирюков <birukovsergej121@gmail.com>
2024-01-07 20:11:02 +0500

    Add logo component

--- src/pages/sign-in-page/sign-in-page.tsx
+++ src/pages/sign-in-page/sign-in-page.tsx
@@ -4,6 +4,7 @@ import {useNavigate} from 'react-router-dom';
 import {useState, MouseEvent} from 'react';
 import {loginAction} from '../../store/apiActions';
 import {appRoutes} from '../../constants';
+import {Logo} from '../../components/logo/logo';
 
 export function SignInPage(): JSX.Element {
   const dispatch = useAppDispatch();
@@ -19,14 +20,7 @@ export function SignInPage(): JSX.Element {
   return (
     <div className="user-page">
       <header className="page-header user-page__head">
-        <div className="logo">
-          <a href="main.html" className="logo__link">
-            <span className="logo__letter logo__letter--1">W</span>
-            <span className="logo__letter logo__letter--2">T</span>
-            <span className="logo__letter logo__letter--3">W</span>
-          </a>
-        </div>
-
+        <Logo/>
         <h1 className="page-title user-page__title">Sign in</h1>
       </header>
 
