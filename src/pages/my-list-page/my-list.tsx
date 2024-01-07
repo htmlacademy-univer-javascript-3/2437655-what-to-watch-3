86310e79b2a119af22fc574b46b47e08af71aa1b
Сергей Бирюков <birukovsergej121@gmail.com>
2024-01-07 12:27:27 +0500

    Add header component

--- src/pages/my-list-page/my-list.tsx
+++ src/pages/my-list-page/my-list.tsx
@@ -1,35 +1,14 @@
 import {Footer} from '../../components/footer/footer.tsx';
 import {Film} from '../../types/film.ts';
 import {FilmsList} from '../../components/films-list/films-list.tsx';
-import {appRoutes} from '../../constants.ts';
-import {Link} from 'react-router-dom';
+import {Header} from '../../components/header/header';
 export type MyListPageProps = {
   films: Film[];
 }
 export function MyListPage({films}: MyListPageProps): JSX.Element {
   return (
     <div className="user-page">
-      <header className="page-header user-page__head">
-        <div className="logo">
-          <Link to={appRoutes.Main} className="logo__link">
-            <span className="logo__letter logo__letter--1">W</span>
-            <span className="logo__letter logo__letter--2">T</span>
-            <span className="logo__letter logo__letter--3">W</span>
-          </Link>
-        </div>
-
-        <h1 className="page-title user-page__title">My list <span className="user-page__film-count">9</span></h1>
-        <ul className="user-block">
-          <li className="user-block__item">
-            <div className="user-block__avatar">
-              <img src="img/avatar.jpg" alt="User avatar" width="63" height="63" />
-            </div>
-          </li>
-          <li className="user-block__item">
-            <a className="user-block__link">Sign out</a>
-          </li>
-        </ul>
-      </header>
+      <Header/>
 
       <section className="catalog">
         <h2 className="catalog__title visually-hidden">Catalog</h2>
